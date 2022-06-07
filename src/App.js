9b5c81ee2bd2bfd8bee2e93e0d138dfa022d82fc
HosseinFalah <98707258+HosseinFalah@users.noreply.github.com>
2022-06-07 13:08:58 +0430

    Get All Crypto a API && Add Feature Search In All Crypto

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
+import { useEffect, useState } from 'react';
 import Navbar from './Components/Navbar';
 import { ThemeProvider } from './Context/ThemeContext';
+import { Route, Routes } from 'react-router-dom';
 import Home from './Routes/Home';
 import Signin from './Routes/Signin';
 import Signup from './Routes/Signup';
 import Account from './Routes/Account';
+import axios from 'axios'
+import './App.css';
 
 const App = () => {
+  const [coins, setCoins] = useState([])
+
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true'
+  
+  useEffect(() => {
+    axios.get(url)
+      .then(response => {
+        setCoins(response.data)
+      })
+  }, [url])
+
   return (
     <ThemeProvider>
       <Navbar/>
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Home coins={coins}/>}/>
         <Route path="/signin" element={<Signin/>}/>
         <Route path="/signup" element={<Signup/>}/>
         <Route path="/account" element={<Account/>}/>
