a97eb8ade6295179c06d5e7b83bf18aa233786d9
HosseinFalah <98707258+HosseinFalah@users.noreply.github.com>
2022-06-07 10:42:35 +0430

    set Routing And Design Component Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './Components/Navbar';
+import { ThemeProvider } from './Context/ThemeContext';
+import Home from './Routes/Home';
+import Signin from './Routes/Signin';
+import Signup from './Routes/Signup';
+import Account from './Routes/Account';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-    </div>
-  );
+    <ThemeProvider>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/signin" element={<Signin/>}/>
+        <Route path="/signup" element={<Signup/>}/>
+        <Route path="/account" element={<Account/>}/>
+      </Routes>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
