3f9cb0676931cbdcf4b37e94d3dfadf146853ddc
hossein <hosseinfalah2021@gmail.com>
2022-12-02 17:14:04 +0330

    increment coin

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = () => {
   const [coins, setCoins] = useState([])
   const [loading, setLoading] = useState(true);
 
-  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=35&page=1&sparkline=true'
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true'
   
   useEffect(() => {
       axios.get(url)
